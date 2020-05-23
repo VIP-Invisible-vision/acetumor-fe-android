f4430f4b980045e4d5474947488ba190b979e1c6
CutePikachu <z5191511@ad.unsw.edu.au>
2020-05-23 16:31:12 +1000

    basic home

--- Acetumor2/app/src/main/java/com/example/acetumor/fragments/HomeFragment.java
+++ Acetumor2/app/src/main/java/com/example/acetumor/fragments/HomeFragment.java
@@ -1,9 +1,16 @@
 package com.example.acetumor.fragments;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+
+
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+
 import com.example.acetumor.R;
 
 /**
@@ -16,6 +23,7 @@ public class HomeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private SearchView search;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -47,10 +55,15 @@ public class HomeFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        search = (SearchView) view.findViewById(R.id.home_search);
+        search.setQueryHint("Search articles");
+
+        return view;
     }
 }
