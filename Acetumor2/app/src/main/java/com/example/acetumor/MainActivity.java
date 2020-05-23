d9258fb70185338dcf0ef96c82e0d0e86375c473
CutePikachu <z5191511@ad.unsw.edu.au>
2020-05-23 14:01:44 +1000

    initial

---
+++ Acetumor2/app/src/main/java/com/example/acetumor/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.acetumor;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.acetumor.fragments.HomeFragment;
+import com.example.acetumor.fragments.InfoFragment;
+import com.example.acetumor.fragments.CompassFragment;
+import com.example.acetumor.fragments.UserFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
+    }
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            openFragment(HomeFragment.newInstance("", ""));
+                            return true;
+                        case R.id.nav_location:
+                            openFragment(CompassFragment.newInstance("", ""));
+                            return true;
+                        case R.id.nav_info:
+                            openFragment(InfoFragment.newInstance("", ""));
+                            return true;
+                        case R.id.nav_user:
+                            openFragment(UserFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
+}
---
+++ Acetumor2/app/src/main/java/com/example/acetumor/NavigationBar.java
@@ -0,0 +1,5 @@
+package com.example.acetumor;
+
+public class NavigationBar {
+
+}
