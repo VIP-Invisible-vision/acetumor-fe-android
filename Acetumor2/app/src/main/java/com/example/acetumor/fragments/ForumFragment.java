a4a27980d16701f6200f911bc406703b22e5e8a8
Queenie <jiyuqi0310@gmail.com>
2020-05-24 00:07:41 +1000

    hard code forum

--- Acetumor2/app/src/main/java/com/example/acetumor/fragments/ForumFragment.java
+++ Acetumor2/app/src/main/java/com/example/acetumor/fragments/ForumFragment.java
@@ -1,12 +1,18 @@
 package com.example.acetumor.fragments;
 
 import android.os.Bundle;
+
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.acetumor.R;
+
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ForumFragment#newInstance} factory method to
@@ -17,9 +23,11 @@ public class ForumFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private SearchView search;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private List<CardView> forumElements;
     public ForumFragment() {
         // Required empty public constructor
     }
@@ -52,8 +60,16 @@ public class ForumFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forum, container, false);
+        View view = inflater.inflate(R.layout.fragment_forum, container, false);
+        search = (SearchView) view.findViewById(R.id.forum_search);
+        search.setQueryHint("Search forums");
+        return view;
     }
 
+    private void createForumElements() {
+        // get data from db
+        // loop through data
+        // build list of elements
+    }
 
 }
