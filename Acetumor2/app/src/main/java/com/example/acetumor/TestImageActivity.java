6041b13db279236c96d6d9b8d1fba1ca6e086e2a
Queenie <jiyuqi0310@gmail.com>
2020-06-02 19:46:17 +1000

    update user feedback

--- Acetumor2/app/src/main/java/com/example/acetumor/TestImageActivity.java
+++ Acetumor2/app/src/main/java/com/example/acetumor/TestImageActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TestImageActivity extends AppCompatActivity {
+public class  TestImageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- Acetumor2/app/src/main/java/com/example/acetumor/UserFeedbackActivity.java
+++ Acetumor2/app/src/main/java/com/example/acetumor/UserFeedbackActivity.java
@@ -2,7 +2,10 @@ package com.example.acetumor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserFeedbackActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class UserFeedbackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_feedback);
+
+        Button submit = findViewById(R.id.user_feedback_submit);
+        Button back = findViewById(R.id.user_feedback_back);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
