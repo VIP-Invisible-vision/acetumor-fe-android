d2ad87f296c46ccd1d43fbb10879efe0f9092bda
calvlinzx <linzexin1214@gmail.com>
2020-07-15 11:44:55 +1000

    update upload activity

--- Acetumor2/app/src/main/java/com/example/acetumor/components/UploadPhotoActivity.java
+++ Acetumor2/app/src/main/java/com/example/acetumor/components/UploadPhotoActivity.java
@@ -3,14 +3,20 @@ package com.example.acetumor.components;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.acetumor.R;
+import com.squareup.picasso.Picasso;
 
 import org.devio.takephoto.model.TImage;
 import org.devio.takephoto.model.TIntentWap;
 import org.devio.takephoto.model.TResult;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class UploadPhotoActivity extends org.devio.takephoto.app.TakePhotoActivity {
@@ -20,7 +26,7 @@ public class UploadPhotoActivity extends org.devio.takephoto.app.TakePhotoActivi
         super.onCreate(savedInstanceState);
         CustomHelper customHelper = new CustomHelper(findViewById(R.id.activity_test_image));
         customHelper.onClick(R.id.test_photo, getTakePhoto());
-//        setContentView(R.layout.activity_test_image);
+        setContentView(R.layout.show_iamge);
     }
 
     @Override
@@ -28,5 +34,26 @@ public class UploadPhotoActivity extends org.devio.takephoto.app.TakePhotoActivi
         super.onResume();
 //        startActivity(new Intent(this, TestImageActivity.class));
     }
+
+    @Override
+    public void takeSuccess(TResult result) {
+//        if (result.getImage().getOriginalPath() != null) {
+//            Log.d("here", "TAKE success!!!!!!!!!!!!!!!!!!!!" + result.getImage().getOriginalPath());
+//        }
+        File imgFile = new File(result.getImage().getOriginalPath());
+
+        ImageView imageView = (ImageView)findViewById(R.id.result_image);
+
+        if (imageView == null) {Log.d("here", "NO!!!!!!!!!!!!!!!!!!!!!!!!!");}
+
+        Picasso.get().load(imgFile).into(imageView);
+    }
+
+    @Override
+    public void takeCancel() {
+        Log.d("here", "TAKE CANCEL");
+        super.onBackPressed();
+    }
+
 
 }
