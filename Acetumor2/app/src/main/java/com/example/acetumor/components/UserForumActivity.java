2e9a3c70499bcefbc6ec65ca9011ef7c401fb3bd
Queenie <jiyuqi0310@gmail.com>
2020-07-22 00:41:29 +1000

    update forum

--- Acetumor2/app/src/main/java/com/example/acetumor/components/UserForumActivity.java
+++ Acetumor2/app/src/main/java/com/example/acetumor/components/UserForumActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.acetumor.MainActivity;
 import com.example.acetumor.R;
@@ -18,6 +19,7 @@ public class UserForumActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_forum);
 
         Button back = findViewById(R.id.user_forum_back);
+        Button post = findViewById(R.id.forum_floating_action_button);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,6 +27,12 @@ public class UserForumActivity extends AppCompatActivity {
                 intent.putExtra(EXTRA_MESSAGE, "user");
                 intent.putExtra("login", true);
                 startActivity(intent);
+            }
+        });
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "To be developed", Toast.LENGTH_SHORT).show();
             }
         });
     }
